d3ba75a85d722a8bcc8c8b64fdc2007f4d6efb92
WafflesMD <124074600+WafflesMD@users.noreply.github.com>
2023-01-31 12:43:13 -0500

    new file

---
+++ 1.py
@@ -0,0 +1,2 @@
+#This will be my first project just to test out integration between Github and Visual Studio Code, which is what will be my primary IDE for learning and exploring the world of Python
+print("My first Python project")
